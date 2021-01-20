17bb3d64b48b793429a1d1f9bb29efd630b69053
Luke Gill <bagrad007@gmail.com>
2021-01-20 16:21:23 -0500

    commit tree dfbc444ec02bafe9f24129b7a0a1eabfa6f14d91

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
+require 'pry'
 # code here!
+class School
+    attr_accessor :roster
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+    def grade(grade)
+        @roster[grade].sort
+    end
+    def sort
+        @roster.each do |grade, student|
+            student.sort!
+        end
+    end
+end
